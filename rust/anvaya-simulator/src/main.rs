de7a869f83c7b70eb434803ef8a023923d13a593
Subesh-Dev <developer.sub@gmail.com>
2026-06-17 22:39:24 +0530

    fix: update anvaya-simulator to use circuit types instead of removed add function

--- rust/anvaya-simulator/src/main.rs
+++ rust/anvaya-simulator/src/main.rs
@@ -1,4 +1,9 @@
+use anvaya_core::circuit::Circuit;
+use anvaya_core::gate::Gate;
+
 fn main() {
-    println!("ANVAYA Simulator starting...");
-    println!("2+2 = {}", anvaya_core::add(2, 2));
+    let mut circuit = Circuit::new(2);
+    circuit.add_gate(Gate::H, vec![0]).unwrap();
+    circuit.add_gate(Gate::CNOT, vec![0, 1]).unwrap();
+    println!("{}", circuit);
 }
