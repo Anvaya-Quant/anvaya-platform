951db9f9a857f3867dac2cd4dea5b30db7ff9e22
Subesh-Dev <developer.sub@gmail.com>
2026-06-17 22:38:51 +0530

    fix: remove dependency on removed add function in anvaya-core-wasm

--- rust/anvaya-core-wasm/src/lib.rs
+++ rust/anvaya-core-wasm/src/lib.rs
@@ -1,7 +1,6 @@
 use wasm_bindgen::prelude::*;
-use anvaya_core;
 
 #[wasm_bindgen]
 pub fn greet(name: &str) -> String {
-    format!("Hello from ANVAYA Core, {}! The sum of 2+2 is {}.", name, anvaya_core::add(2, 2))
+    format!("Hello from ANVAYA Core, {}!", name)
 }
